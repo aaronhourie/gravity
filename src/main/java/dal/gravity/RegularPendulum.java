e541652c06bfb244fc2d05ba4347228ed5055a75
aaron <aaronhourie@gmail.com>
2017-03-29 21:12:28 -0300

    Collapsed Hierarchy to remove AbstractEarthPendulum

--- src/main/java/dal/gravity/RegularPendulum.java
+++ src/main/java/dal/gravity/RegularPendulum.java
@@ -3,7 +3,7 @@ package dal.gravity;
 /**
  * Represents a pendulum
  */
-public class RegularPendulum extends AbstractEarthPendulum {
+public class RegularPendulum extends AbstractPendulum {
     private double delta, iterations = 0;
     private double dissipation;
     private double lastTheta, lastVel, lastAccel;
@@ -12,25 +12,25 @@ public class RegularPendulum extends AbstractEarthPendulum {
      * Creates a new Pendulum instance 
      */
     public RegularPendulum (double inLength, double inMass, double inTheta0, 
-		     double inDelta, double inDiss) {
-	super (inLength, inMass, inTheta0);
-	delta=inDelta;
-	dissipation = inDiss;
-	lastVel = 0;
-	lastTheta = this.getMaxAngularDisplacement ();
-	lastAccel = -(this.getGravitationalField () / this.getStringLength ())*Math.sin (lastTheta);
+            double inDelta, double inDiss, double gravity) {
+        super (inLength, inMass, inTheta0, gravity);
+        delta=inDelta;
+        dissipation = inDiss;
+        lastVel = 0;
+        lastTheta = this.getMaxAngularDisplacement ();
+        lastAccel = -(this.getGravitationalField () / this.getStringLength ())*Math.sin (lastTheta);
     }
 
     public RegularPendulum (double inLength, double inMass, double inTheta0, 
-		     double inDelta) {
-	this (inLength, inMass, inTheta0, inDelta, 0);
+            double inDelta, double gravity) {
+        this (inLength, inMass, inTheta0, inDelta, 0, gravity);
     }
 
     public void step () {
-	iterations++;
-	lastTheta = lastTheta + lastVel*delta;
-	lastVel = lastVel + lastAccel*delta;
-	lastAccel = - dissipation*lastVel - this.getGravitationalField () / this.getStringLength () * Math.sin (lastTheta);
+        iterations++;
+        lastTheta = lastTheta + lastVel*delta;
+        lastVel = lastVel + lastAccel*delta;
+        lastAccel = - dissipation*lastVel - this.getGravitationalField () / this.getStringLength () * Math.sin (lastTheta);
     }
 
     public double getLastTheta () { return lastTheta; }
@@ -38,6 +38,6 @@ public class RegularPendulum extends AbstractEarthPendulum {
     public double getLastAcceleration () { return lastAccel; }
     public double getLastTime () { return iterations*delta; }
     public double getDissipationConstant () { return dissipation; }
-    
+
 
 }
